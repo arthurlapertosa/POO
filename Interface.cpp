5df721d5f2d3f97fde0eea6a32f296adfadbbfae
Rafaela Moreira <moreiramrafaela@gmail.com>
2019-09-30 22:15:56 -0300

    arquivos da classe interface criado

---
+++ Interface.cpp
@@ -0,0 +1,18 @@
+/*
+ * Interface.cpp
+ *
+ *  Created on: 30 de set de 2019
+ *      Author: rafaelamoreira
+ */
+
+#include "Interface.h"
+
+Interface::Interface() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Interface::~Interface() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Interface.h
@@ -0,0 +1,17 @@
+/*
+ * Interface.h
+ *
+ *  Created on: 30 de set de 2019
+ *      Author: rafaelamoreira
+ */
+
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
+class Interface {
+public:
+	Interface();
+	virtual ~Interface();
+};
+
+#endif /* INTERFACE_H_ */
